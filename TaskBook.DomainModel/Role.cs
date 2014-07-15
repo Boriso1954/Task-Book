39b4efe7f1fb700e951049c76bedffd7ee107cbe
Boriso1954 <b.obuhov@outlook.com>
2014-07-15 16:40:37 +0300

    Add DomainModel and DAL projects

---
+++ TaskBook.DomainModel/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TaskBook.DomainModel
+{
+    public class Role: IdentityRole
+    {
+        [Required]
+        [MaxLength(256, ErrorMessage = "Description should be up to 256 characters long.")]
+        public string Description { get; set; }
+    }
+}
---
+++ TaskBook.DomainModel/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TaskBook.DomainModel
+{
+    public class User: IdentityUser
+    {
+        [Required]
+        [MaxLength(25, ErrorMessage = "FirstName shall be from 1 to 25 characters long.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(25, ErrorMessage = "LastName shall be from 1 to 25 characters long.")]
+        public string LastName { get; set; }
+    }
+}
