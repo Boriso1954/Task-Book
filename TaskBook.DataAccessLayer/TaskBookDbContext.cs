e275de67e577b67329a9a6d976209549659f0198
Boriso1954 <b.obuhov@outlook.com>
2014-07-15 16:57:19 +0300

    Enable migration

---
+++ TaskBook.DataAccessLayer/TaskBookDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TaskBook.DomainModel;
+
+namespace TaskBook.DataAccessLayer
+{
+    public class TaskBookDbContext: IdentityDbContext<User>
+    {
+        public TaskBookDbContext()
+            : base("TaskBookDbContext")
+        {
+            // This makes sure that the entities are retrieved as objects of their respective classes
+            // instead of proxies–making debugging much easier.
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Model configurations here
+        }
+    }
+}
---
+++ TaskBook.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskBook.WebApi.WebApiApplication" Language="C#" %>
