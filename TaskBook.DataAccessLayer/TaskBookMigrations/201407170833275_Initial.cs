459dde51e0e619c4ded07baa3b9183e72c2b0caa
Boriso1954 <b.obuhov@outlook.com>
2014-07-17 11:36:25 +0300

    Add Aiuth and TaskBook migrations

---
+++ TaskBook.DataAccessLayer/TaskBookMigrations/201407170833275_Initial.cs
@@ -0,0 +1,27 @@
+namespace TaskBook.DataAccessLayer.TaskBookMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tasks",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 64),
+                        RowVersion = c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Tasks");
+        }
+    }
+}
