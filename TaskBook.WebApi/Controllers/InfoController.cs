7a87056f51f2afee61ae5c3f393e7a1270d9279a
Boriso2014 <b.obuhov@outlook.com>
2015-08-27 00:16:23 +0300

    Version info

---
+++ TaskBook.WebApi/Controllers/InfoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NLog.Mvc;
+
+namespace TaskBook.WebApi.Controllers
+{
+    [RoutePrefix("api/Info")]
+    public class InfoController : ApiController
+    {
+        private readonly ILogger _logger;
+
+        public InfoController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/Info/GetAppVersion
+        [Route("GetAppVersion")]
+        [ResponseType(typeof(string[]))]
+        public IHttpActionResult GetAppVersion()
+        {
+            string[] info = new string[2];
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            info[0] = assembly.GetName().Version.ToString();
+            var date = System.IO.File.GetLastWriteTime(assembly.Location).ToUniversalTime();
+            info[1] = string.Format("{0: MMMM dd, yyyy HH:mm:ss} UTC", date);
+
+            return Ok(info);
+        }
+
+    }
+}
--- TaskBook.WebApi/Scripts/app/controllers/layoutController.js
+++ TaskBook.WebApi/Scripts/app/controllers/layoutController.js
@@ -1,5 +1,17 @@
 ﻿"use strict";
-app.controller("layoutController", ["$scope", "$location", "authService", function ($scope, $location, authService) {
+app.controller("layoutController", ["$scope", "$location", "$http", "authService", function ($scope, $location, $http, authService) {
+
+    $scope.version = "";
+    $scope.created = "";
+    var getAppInfo = function () {
+        $http.get("api/Info/GetAppVersion")
+            .then(function (result) {
+                $scope.version = result.data[0];
+                $scope.created = result.data[1];
+            });
+    };
+
+    getAppInfo();
 
     $scope.logOut = function () {
         authService.logOut();
